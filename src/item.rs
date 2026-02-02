5c8c6eaa8119b61e67e00303876b685b13798675
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-02 12:39:01 -0800

    add type for items.

---
+++ src/item.rs
@@ -0,0 +1,138 @@
+use std::fmt::format;
+
+use uuid::Uuid;
+
+use crate::priority::Priority;
+
+/// structure that represents an item in a wishlist
+
+#[derive(PartialEq, Eq)]
+pub struct Item {
+    pub id: String,
+    pub name: String,
+    pub quantity: u32,
+    pub priority: Priority,
+    pub url: String,
+    pub notes: Vec<String>
+}
+
+impl Item {
+    /**
+     * This method creates a builder that will help facilate in creating
+     * a wish list item.
+     * 
+     * Items can then be created like this:
+     * 
+     * let nintendo_switch_2 = Item::builder()
+     * .set_name("Nintendo Switch 2")
+     * .set_quantity(1)
+     * .set_priority("high")
+     * .set_url("https://example.com/nintendo-switch-2")
+     * .build();
+     */
+    pub fn builder() -> ItemBuilder {
+        ItemBuilder::new()
+    }
+
+    /// this method utilizes the builder method to create an an empty item.
+    pub fn new() -> Self {
+        Item::builder().build()
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{}\t{}\t{}\t{}\t{}\r\n\r\n{}", 
+        self.id, 
+        self.name, 
+        self.quantity, 
+        self.priority.to_string(), 
+        self.url)
+    }
+}
+
+pub struct ItemBuilder {
+    pub id: String,
+    pub name: Option<String>,
+    pub quantity: Option<u32>,
+    pub priority: Option<Priority>,
+    pub url: Option<String>,
+    pub notes: Vec<String>
+}
+
+impl ItemBuilder {
+    pub fn new() -> Self {
+        ItemBuilder {
+            id: Uuid::new_v4().hyphenated().encode_upper(&mut Uuid::encode_buffer()).to_string(),
+            name: None,
+            quantity: None,
+            priority: None,
+            url: None,
+            notes: vec![]
+        }
+    }
+
+    /**
+     * set the identifier of an item.
+     * If this is unused, a default value will be provided.
+     */
+    pub fn set_id(&mut self, id: &str) -> &mut Self {
+        self.id = id.to_owned();
+        self
+    }
+
+    pub fn set_name(&mut self, name: &str) -> &mut Self {
+        self.name = Some(name.to_owned());
+        self
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) -> &mut Self {
+        self.quantity = Some(quantity);
+        self
+    }
+
+    pub fn set_priority(&mut self, priority: &str) -> &mut Self {
+        self.priority = Priority::from_str(priority);
+        self
+    }
+
+    pub fn set_url(&mut self, url: &str) -> &mut Self {
+        self.url = Some(url.to_owned());
+        self
+    }
+
+    pub fn add_note(&mut self, note: &str) -> &mut Self {
+        self.notes.push(note.to_owned());
+        self
+    }
+
+    /**
+     * this method is used to actually build the wishlist item.
+     * any items not provided a value with the respective set methods
+     * will be given a default value.
+     */
+    pub fn build(&self) -> Item {
+        Item {
+            id: self.id.clone(),
+            name: if let Some(ref name) = self.name {
+                name.to_owned()
+            } else {
+                String::default()
+            },
+            quantity: if let Some(ref quantity) = self.quantity {
+                quantity.to_owned()
+            } else {
+                1
+            },
+            priority: if let Some(priority) = self.priority {
+                priority
+            } else {
+                Priority::Medium
+            },
+            url : if let Some(ref url) = self.url {
+                url.to_owned()
+            } else {
+                String::default()
+            },
+            notes: self.notes.clone()
+        }
+    }
+}
