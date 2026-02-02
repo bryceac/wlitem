7b6fb7f6f400f8e9753170b77b3e8e566fbdca5f
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-02 14:30:39 -0800

    work on error type for string parsing.

--- src/item_parsing_error.rs
+++ src/item_parsing_error.rs
@@ -1,4 +1,21 @@
+use core::fmt;
+
 pub enum ItemParsingError {
-    TooFewItems(u32),
-    TooManyItems(u32)
+    TooFewFields(u32),
+    TooManyFields(u32)
+}
+
+impl ItemParsingError {
+    pub fn to_string(&self) -> String {
+        match self {
+            ItemParsingError::TooFewFields(fields) => format!("Item contains {} fields, which is not enough. Items MUST contain 5 fields.", fields),
+            ItemParsingError::TooManyFields(fields) => format!("Item contains {} fields, which is too many. Items MUST contain 5 fields.", fields)
+        }
+    }
+}
+
+impl fmt::Display for ItemParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
 }
