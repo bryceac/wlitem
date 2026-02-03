7e234badf2710fd3616785e5f4c25c4521d46812
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-03 13:46:22 -0800

    make sure items can be exported as TSV.

--- src/save_vec.rs
+++ src/save_vec.rs
@@ -5,6 +5,7 @@ use std::{fs::File, io::{ Write, Error } };
 use serde_json;
 pub trait Save {
     fn save(&self, path: &str) -> Result<(), Error>;
+    fn save_tsv(&self, path: &str) -> Result<(), Error>;
 }
 
 impl Save for Vec<Item> {
@@ -13,6 +14,32 @@ impl Save for Vec<Item> {
         let json_sring = serde_json::to_string_pretty(self)?;
 
         match write!(output, "{}", json_sring) {
+            Ok(()) => Ok(()),
+            Err(error) => Err(error)
+        }
+    }
+
+    fn save_tsv(&self, path: &str) -> Result<(), Error> {
+        let mut items = String::new();
+
+        for item in self {
+            let item_string = format!("{}\t{}\t{}\t{}\t{}\r\n", 
+            item.id, 
+            item.name, 
+            item.quantity, 
+            item.priority.to_str(), 
+            if let Some(ref url) = item.url {
+                url.as_str()
+            } else {
+                ""
+            });
+
+            items.push_str(&item_string);
+        }
+
+        let mut output = File::create(path)?;
+
+        match write!(output, "{}", items) {
             Ok(()) => Ok(()),
             Err(error) => Err(error)
         }
