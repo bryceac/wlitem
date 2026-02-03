75ccf19581971467c04d90a4eaa8c7d7a187819c
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-03 13:07:29 -0800

    fix filename used.

--- src/lib.rs
+++ src/lib.rs
@@ -104,8 +104,8 @@ mod tests {
             ])
         ];
 
-        let decoded_items = Item::from_file("test.json");
+        let decoded_items = Item::from_file("wishlist.json");
 
-        assert_eq!(decoded_items.unwrap(), expected_items)
+        assert_eq!(decoded_items.ok().unwrap(), expected_items)
     }
 }
