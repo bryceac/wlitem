cc8fabdddd856c237557f17c2761eaeb13b65625
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-05 17:44:21 -0800

    continue working on making an error type.

--- src/priority_parse_error.rs
+++ src/priority_parse_error.rs
@@ -1,3 +1,20 @@
-pub enum PriorityParseError {
+use std::{ error::Error, fmt };
 
+#[derive(Debug)]
+pub enum PriorityParseError {
+    InvalidPriority(String)
 }
+
+impl fmt::Display for PriorityParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::InvalidPriority(s) => {
+                let error_string = format!("{} is not a valid priority level", s);
+
+                write!(f, "{}", error_string)
+            }
+        }
+    }
+}
+
+impl Error for PriorityParseError {}
